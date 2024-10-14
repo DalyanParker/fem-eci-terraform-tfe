be35b4530d71e9b70a2cb45a63fbd519ea34fda1
Dalyan Parker <dalyan@quiver.co.uk>
2024-10-14 18:40:56 +0100

    Set up vcs automation

--- main.tf
+++ main.tf
@@ -15,11 +15,17 @@ module "workspace" {
     source  = "ALT-F4-LLC/workspace/tfe"
     version = "0.6.0"
 
-    execution_mode    = "local"
+    execution_mode    = each.value.execution_mode
     description       = each.value.description
     name              = each.key
     organization_name = var.organization_name
     project_id        = each.value.project_id
+
+    vcs_repo = {
+        github_app_installation_id = data.tfe_github_app_installation.this.id
+        identifier = each.value.vcs_repo_identifier
+    }
+
 }
 
 moved {
--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "organization_name" {
-  default = "DalyanParker"
-  type    = string
+    default = "DalyanParker"
+    type    = string
+}
+
+variable "github_app_installation_id"{
+    default = 55964077
+    type    = number
+}
+
+variable "github_app_installation_name" {
+    default = "DalyanParker"
+    type    = string
 }
